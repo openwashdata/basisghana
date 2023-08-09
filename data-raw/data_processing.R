523d852ff3f85d8e6705dfbbd0bf5eeaee433456
Lars Schöbitz <lschoebitz@ethz.ch>
2023-08-09 13:32:23 +0200

    document script

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,36 +1,49 @@
+# description -------------------------------------------------------------
 
+
+
+# r packages --------------------------------------------------------------
 
 library(tabulizer)
 library(dplyr)
 library(tidyr)
 
+# pdf path ---------------------------------------------------------------
+
 basisghana_pdf <- "data-raw/ODF_Communities_-September_2017_1 (2).pdf"
 
-extract_tables(basisghana_pdf,
-               pages = 1,
-               guess = FALSE,
-               columns = list(c(4, 5, 6, 7, 8, 9)))
+# page 01 ----------------------------------------------------------------
 
+# define area to extract data from. only use columns starting from
+# "area council"
 df01 <- extract_tables(basisghana_pdf,
                        pages = 1,
                        guess = FALSE,
-                       area = list(c(120, 200, 400, 800))) |>  # (top,left,bottom,right)
+                       # (top, left, bottom,right)
+                       area = list(c(120, 200, 400, 800))) |>
   as.data.frame() |>
+  # add a running id to enable a join later on
   mutate(no = seq(1:n())) |>
   relocate(no) |>
   as_tibble() |>
   mutate(across(X4:X8, as.numeric))
 
-names(df01) <- c("no","area_council", "partner", "community_name", "population", "households", "toilets", "hwf", "odfs")
+# define column names
+col_names <- c("no","area_council", "partner", "community_name", "population", "households", "toilets", "hwf", "odfs")
 
+names(df01) <- col_names
+
+# define area to extract data from. only use columns No. and District
 m01_district <- extract_tables(basisghana_pdf,
                                pages = 1,
                                guess = FALSE,
-                               area = list(c(100, 0, 400, 200)))     # (top,left,bottom,right)
+                               # (top,left,bottom,right)
+                               area = list(c(100, 0, 400, 200)))
 
+# add colnames
+colnames(m01_district[[1]]) <- "no. districts"
 
-colnames(m01_district[[1]]) <- paste0("no. districts")
-
+# wrangle data get a complete dataframe for no. and district
 df01_district <- m01_district |>
   as.data.frame() |>
   as_tibble() |>
@@ -41,12 +54,17 @@ df01_district <- m01_district |>
   fill(district, .direction = "down") |>
   mutate(no = as.numeric(no))
 
+
+# join the no and district with data
 page01 <- df01_district |>
   left_join(df01)
 
-basisghana <- page01
 
-## code to prepare `DATASET` dataset goes here
+
+
+# export final dataframe --------------------------------------------------
+
+basisghana <- page01
 
 usethis::use_data(basisghana, overwrite = TRUE)
 fs::dir_create(here::here("inst", "extdata"))
