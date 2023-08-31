a3fc163c192973ed83658b747f8a1ca0361ea2c6
Lars Schöbitz <lschoebitz@ethz.ch>
2023-08-31 11:25:11 +0200

    Initial commit

---
+++ R/basisghana.R
@@ -0,0 +1,18 @@
+#' Open defecation free (ODF) communities in Ghana
+#'
+#' TODO: Description
+#'
+#' @format A tibble with 2784 rows and 7 variables:
+#' \describe{
+#'   \item{no}{A running id for unique for each region.}
+#'   \item{region}{First level of subnational government administration within the Republic of Ghana.}
+#'   \item{district}{Second level administrative subdivision below region.}
+#'   \item{area_council}{Third level administrative unit below district level.}
+#'   \item{community}{Open defecation free (ODF) community. The community is the smallest level of local administration in Ghana (also called unit committees).}
+#'   \item{partner}{Implementing partner of the CLTS (Community-Led Total Sanitation) program in the respective community.}
+#'   \item{population}{Populationg size of the respective community.}
+#'   \item{households}{Number of households in the respective community.}
+#'   \item{toilets}{Number of toilets in the respective community.}
+#'   \item{hwf}{??? TODO}
+#' }
+"basisghana"
