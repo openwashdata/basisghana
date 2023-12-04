739415a122e089f18cc29ba773c07ff635597168
Skorik  Sophia <sskorik@ethz.ch>
2023-12-04 16:26:23 +0100

    added description

--- R/basisghana.R
+++ R/basisghana.R
@@ -1,18 +1,18 @@
-#' Open defecation free (ODF) communities in Ghana
+#' Basic Sanitation Information System - BaSIS
 #'
-#' TODO: Description
+#' The BaSIS dataset captures sanitation information across Ghana's subnational regions,
+#' focusing on open defection free communities data such as population, households, toilets, and handwashing facilities.
 #'
-#' @format A tibble with 2784 rows and 7 variables:
+#' @format A tibble with 1618 rows and 10 variables:
 #' \describe{
-#'   \item{no}{A running id for unique for each region.}
-#'   \item{region}{First level of subnational government administration within the Republic of Ghana.}
-#'   \item{district}{Second level administrative subdivision below region.}
-#'   \item{area_council}{Third level administrative unit below district level.}
-#'   \item{community}{Open defecation free (ODF) community. The community is the smallest level of local administration in Ghana (also called unit committees).}
-#'   \item{partner}{Implementing partner of the CLTS (Community-Led Total Sanitation) program in the respective community.}
-#'   \item{population}{Populationg size of the respective community.}
-#'   \item{households}{Number of households in the respective community.}
-#'   \item{toilets}{Number of toilets in the respective community.}
-#'   \item{hwf}{??? TODO}
+#'   \item{ no}{ A running id for unique for each region.}
+#'   \item{ region}{ First level of subnational government administration within the Republic of Ghana.}
+#'   \item{ district}{ Second level administrative subdivision below region.}
+#'   \item{ area_council}{ Third level administrative unit below district level.}
+#'   \item{ community}{ Open defecation free (ODF) community. The community is the smallest level of local administration in Ghana (also called unit committees).}
+#'   \item{ partner}{ Implementing partner of the CLTS (Community-Led Total Sanitation) program in the respective community.}
+#'   \item{ population}{ Population size of the respective community.}
+#'   \item{ households}{ Number of households in the respective community.}
+#'   \item{ toilets}{ Number of toilets in the respective community.}
+#'   \item{ hwf}{ Number of handwashing facilities in the respective community.}
 #' }
-"basisghana"
---
+++ R/ghanapop.R
@@ -0,0 +1,17 @@
+#' Ghana Population
+#'
+#' This dataset contains demographic details by district in Ghana, including population breakdown by gender
+#' and total population counts. It offers insights into regional demographics and gender distribution across Ghana's districts.
+#'
+#' @format A tibble with 260 rows and 9 variables:
+#' \describe{
+#'   \item{ country_en}{ The English name denoting the country where the data was gathered (Ghana).}
+#'   \item{ country_pcode}{The ISO2 code representing Ghana (country of data collection).}
+#'   \item{ region_en}{ The English designation specifying the region within Ghana. (Ghana comprises 16 distinct regions.)}
+#'   \item{ region_pcode}{ The postal code linked to the specific region.}
+#'   \item{ district_en}{The English name identifying the district in Ghana. (There are 261 districts in Ghana.)}
+#'   \item{ district_pcode}{ The postal code specific to the district.}
+#'   \item{ female_pop}{ Total female population within the corresponding District.}
+#'   \item{ male_pop}{ Total male population within the corresponding District.}
+#'   \item{ total_pop}{ Total population within the corresponding District.}
+#' }
